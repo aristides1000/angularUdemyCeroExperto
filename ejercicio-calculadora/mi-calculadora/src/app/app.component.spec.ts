65e4080d2cd0a9dadad32de1b067ee7803cf7578
aristides1000 <42152103+aristides1000@users.noreply.github.com>
2019-09-03 09:03:42 -0400

    mi calculadora

--- ejercicio-calculadora/mi-calculadora/src/app/app.component.spec.ts
+++ ejercicio-calculadora/mi-calculadora/src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('mi-calculadora');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mi-calculadora!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mi-calculadora app is running!');
   });
 });
---
+++ ejercicio-calculadora/mi-calculadora/src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent implements OnInit {
+
+  title = 'Aplicación de Calculadora';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
