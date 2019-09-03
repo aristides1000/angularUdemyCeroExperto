c4b7561819ccd9e1c764ecbf7627b85dc8a0e5c2
aristides1000 <42152103+aristides1000@users.noreply.github.com>
2019-09-03 09:08:16 -0400

    video 18 mi-calculadora

--- ejercicio-calculadora/mi-calculadora/e2e/src/app.e2e-spec.ts
+++ ejercicio-calculadora/mi-calculadora/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to mi-calculadora!');
+    expect(page.getTitleText()).toEqual('mi-calculadora app is running!');
   });
 
   afterEach(async () => {
