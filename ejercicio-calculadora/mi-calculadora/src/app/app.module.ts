2dc2f61d78e4ec32cd0fd6be6463101ff3700784
aristides1000 <42152103+aristides1000@users.noreply.github.com>
2019-09-04 14:10:13 -0400

    19. Solución: Calculadora con Angular

--- ejercicio-calculadora/mi-calculadora/src/app/app.module.ts
+++ ejercicio-calculadora/mi-calculadora/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CalculadoraComponent } from './calculadora/calculadora.component';
     CalculadoraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ejercicio-calculadora/mi-calculadora/src/app/calculadora/calculadora.component.ts
+++ ejercicio-calculadora/mi-calculadora/src/app/calculadora/calculadora.component.ts
@@ -5,19 +5,42 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
 })
+
 export class CalculadoraComponent implements OnInit {
 
   title = 'Aplicación de Calculadora';
 
+  mensajeSuma = null;
+
+  operandoA = null;
+  operandoB = null;
+
+
   efectuarSuma = true;
 
   constructor() {
-    const efectuarSuma = false;
-    if (efectuarSuma === false) {
+    /* const efectuarSuma = false; */
+    if ((this.operandoA === null) && (this.operandoB === null)) {
       this.efectuarSuma = true;
     } else {
       this.efectuarSuma = false;
     }
+  }
+
+  onSumar() {
+    this.mensajeSuma = parseFloat(this.operandoA) + parseFloat(this.operandoB);
+  }
+
+  /* onRealizarSuma(event: Event) {
+    this.resultadoSuma = (<HTMLInputElement>event.target).value;
+  } */
+
+  onCapturaOperandoA(){
+    this.operandoA = (<HTMLInputElement>event.target).value;
+  }
+
+  onCapturaOperandoB(){
+    this.operandoB = (<HTMLInputElement>event.target).value;
   }
 
   ngOnInit() {
