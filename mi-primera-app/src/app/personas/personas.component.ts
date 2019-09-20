0197443f640604256fddb5cbde15120bdc8a7a11
aristides1000 <42152103+aristides1000@users.noreply.github.com>
2019-09-20 15:46:51 -0400

    021 Directiva else con ngIf en Angular

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-personas',
     templateUrl: './personas.component.html',
-    /* Aquí puedo agregar una plantilla html cambiando la propiedad del decorador y podemos escribir directamente código html donde dice template  */
+    /* Aquí puedo agregar una plantilla html cambiando la propiedad
+    del decorador y podemos escribir directamente código html donde dice template  */
     /*template: '<h1>Listado de Personas</h1><app-persona></app-persona><app-persona></app-persona>' */
     /*Ahora bien, con esta sintaxis tengo que tener todo el código html en una sola línea*/
     /* Ahora bien, si queremos tener más de una línea de código debemos cambiar de comillas simples a comillas invertidas */
@@ -19,32 +20,37 @@ import { Component } from '@angular/core';
     h1{
         color:blue;
     }
-    `]/* si vamos a agregar una sola línea de estilos aplicamos comillas simples '' si es más de una línea lo hacemos con comillas simples invertidas `` y con ello ya podemos darle al botón enter dentro del arreglo */
+    `]/* si vamos a agregar una sola línea de estilos aplicamos comillas
+    simples '' si es más de una línea lo hacemos con comillas simples
+    invertidas `` y con ello ya podemos darle al botón enter dentro del arreglo */
 })
 export class PersonasComponent {
     agregarPersona = false;
 
     /* Esta parte responde a Event Binding */
-    agregarPersonaStatus = "No se ha agregado ninguna persona";
-    /* Ahora lo que se desea es responder a un evento, que en este caso se llamara  */
+    agregarPersonaStatus = 'No se ha agregado ninguna persona';
+    /* Ahora lo que se desea es responder a un evento, que en este caso se llamara */
     /* Hasta aquí la parte de Event Binding */
 
     /* Ahora bien, lo que se quiere es que dinámicamente el botón cambie de apariencia de deshabilitado a habilitado */
-    
-    tituloPersona = "Ingeniero"; /* POr aquí es por dónde nos traemos la información que desamos capturar esto me sirve para el desarrollo de la aplicación de la empresa */
-    
+
+    tituloPersona = 'Ingeniero'; /* POr aquí es por dónde nos traemos
+    la información que desamos capturar esto me sirve para el desarrollo de la aplicación de la empresa */
+
     personaCreada = false;
 
     /* Es por ello que vamos a simular una acción dentro de nuestra clase personas component lo que vamos a crear es un constructor */
     constructor() {
         setTimeout(
             /* Aquí vamos a proceder a llamar a una función flecha */
-            () => {/* y dentro de esta función flecha vamos a proceder a modificar nuestra propiedad agregarPersona y en lugar de que sea el valir false vamos a cambiarlo a true*/
+            () => {
+                /* y dentro de esta función flecha vamos a proceder a modificar
+                nuestra propiedad agregarPersona y en lugar de que sea el valir false vamos a cambiarlo a true */
                 this.agregarPersona = true;
             }
-            , 3000);/*Esta es una función de javascript*/
-            /* Podemos observar que después de los 3 segundos se habilita nuestro botón y pasa a true */
-            /* Así que con lo que acabamos de ver aquí ya estamos trabajando con nuestro concepto de Property Binding */
+            , 3000); /* Esta es una función de javascript */
+        /* Podemos observar que después de los 3 segundos se habilita nuestro botón y pasa a true */
+        /* Así que con lo que acabamos de ver aquí ya estamos trabajando con nuestro concepto de Property Binding */
     }
 
     onCrearPersona() {
@@ -56,6 +62,6 @@ export class PersonasComponent {
     /* El siguente método es el que transcribe todo los que copiamos en un input */
 
     onModificarPersona(event: Event) {
-        this.tituloPersona = (<HTMLInputElement>event.target).value;
+        this.tituloPersona = (event.target as HTMLInputElement).value;
     }
 }
